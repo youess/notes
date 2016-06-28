b892c91666a9aacdeb4cae25314cf579711460b5
denis <denis_lton@hotmail.com>
2016-06-28 15:11:47 +0000

    add test result

--- js/async/test.js
+++ js/async/test.js
@@ -3,6 +3,6 @@
 var s = "123 ",
     p = "few"
 
-console.log(s + p);
-console.log(s.trim() + p);
-console.log(s + p);
+console.log(s + p);        // "123 few"
+console.log(s.trim() + p); // "123fw"
+console.log(s + p);        // "123 few"
